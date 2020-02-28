866010886e94c78426281dfdf7267722aca14b3f
Ahmad Wilson <zer0ne.io.x@gmail.com>
2020-02-27 22:07:51 -0500

    enable build/test matrix based on OS_VERSION env

--- test/integration_tests.sh
+++ test/integration_tests.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
 # [Test-Setup]
-for test_os in centos:7 centos:8 fedora:28 fedora:29 fedora:30 fedora:31 debian:8; do
-  docker build --file Containerfile --build-arg OS_VERSION=$test_os --tag testing-$test_os .
-done
+cat <<OS | xargs -I % docker build --file Containerfile --build-arg OS_VERSION=% --tag testing-% .
+centos:7
+centos:8
+fedora:28
+fedora:29
+fedora:30
+fedora:31
+debian:8
+OS
 
 # [Test-Run+Validate]
-for test_os in centos:7 centos:8 fedora:28 fedora:29 fedora:30 fedora:31 debian:8; do
-  GOSS_FILES_PATH=test dgoss run --env-file test/unit-test-service.env testing-$test_os sleep 30
-done
-
+export GOSS_FILES_PATH=test
+cat <<OS | xargs -I % dgoss run --env-file test/unit-test-service.env testing-% sleep 30
+centos:7
+centos:8
+fedora:28
+fedora:29
+fedora:30
+fedora:31
+debian:8
+OS
+unset GOSS_FILES_PATH
