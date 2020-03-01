08cecadde7c85ed09e5956e19fbf67798bfc40b1
Ahmad Wilson <zer0ne.io.x@gmail.com>
2020-03-01 17:52:16 -0500

    update local integration test script

--- test/integration_tests.sh
+++ test/integration_tests.sh
@@ -6,25 +6,29 @@ set -euo pipefail
 [ -n "${DEBUG:-""}" ] && set -x
 
 # [Test-Setup]
-cat <<OS | xargs -I % docker build --file Containerfile --build-arg OS_VERSION=% --tag testing-% .
-centos:7
-centos:8
-fedora:28
-fedora:29
-fedora:30
-fedora:31
-debian:8
+cat <<OS | xargs -I % docker build --file builds/artifacts/%/Containerfile --tag testing-systemd:% .
+centos-7
+centos-8
+fedora-29
+fedora-30
+fedora-31
+ubuntu-18.04
+ubuntu-19.04
+debian-8
+debian-9
 OS
 
 # [Test-Run+Validate]
 export GOSS_FILES_PATH=test
-cat <<OS | xargs -I % dgoss run --env-file test/unit-test-service.env testing-% sleep 30
-centos:7
-centos:8
-fedora:28
-fedora:29
-fedora:30
-fedora:31
-debian:8
+cat <<OS | xargs -I % dgoss run --env-file test/unit-test-service.env testing-systemd:% sleep 30
+centos-7
+centos-8
+fedora-29
+fedora-30
+fedora-31
+ubuntu-18.04
+ubuntu-19.04
+debian-8
+debian-9
 OS
 unset GOSS_FILES_PATH
